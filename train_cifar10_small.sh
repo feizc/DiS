5ed90a8a86c1953d0aefbdf53819320ec4ee6932
费政聪 <3300949577@qq.com>
2024-02-03 21:04:03 -0600

    Delete train_cifar10_small.sh

--- train_cifar10_small.sh
+++
@@ -1,6 +0,0 @@
-torchrun --nnodes=1 --nproc_per_node=8 train.py \
---model DiS-S/2 \
---data-path /TrainData/Multimodal/zhengcong.fei/dis/data \
---image-size 32 \
---task-type class-cond \
---num-classes 10
